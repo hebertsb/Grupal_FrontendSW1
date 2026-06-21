6108cacae8fb6eb679d9de0dcdfd1f42616236f6
Luis F Blanco <blanco.luis@ficct.uagrm.edu.bo>
2026-06-21 04:39:44 -0400

    Fix Circular Dependency with Auth Service

--- src/app/nucleo/interceptores/jwt.interceptor.ts
+++ src/app/nucleo/interceptores/jwt.interceptor.ts
@@ -3,8 +3,7 @@ import { inject } from '@angular/core';
 import { AutenticacionServicio } from '../../compartido/servicios/autenticacion.servicio';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const auth  = inject(AutenticacionServicio);
-  const token = auth.obtenerToken();
+  const token = localStorage.getItem('sivic_token');
 
   if (token) {
     req = req.clone({
